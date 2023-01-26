5c9b4d7624ab346ccd39522e96352ee4dc9865ac
Jenzi <jennydinh94@gmail.com>
2023-01-26 12:06:53 +0100

    Create middlewares

--- AnimalAdoption/Program.cs
+++ AnimalAdoption/Program.cs
@@ -1,7 +1,16 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
 
 app.Run();
-
