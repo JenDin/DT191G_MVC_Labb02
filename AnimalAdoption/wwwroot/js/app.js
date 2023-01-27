187e7774f1978a31e44c90241ebfde129e403d79
Jenzi <jennydinh94@gmail.com>
2023-01-27 12:19:03 +0100

    Create header and footer

--- AnimalAdoption/wwwroot/js/app.js
+++ AnimalAdoption/wwwroot/js/app.js
@@ -1,1 +1,13 @@
-﻿
+﻿const mainMenu = document.querySelector("#nav-list");
+const toggler = document.querySelector("#toggle");
+const hamburgerIcon = document.querySelector(".fa-bars");
+
+toggler.addEventListener("click", function () {
+    mainMenu.classList.toggle("active");
+
+    if (hamburgerIcon.classList.contains("fa-bars")) {
+        hamburgerIcon.classList.replace("fa-bars", "fa-times")
+    } else {
+        hamburgerIcon.classList.replace("fa-times", "fa-bars")
+    }
+})
