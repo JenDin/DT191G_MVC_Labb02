9a122b507e4d4cc75d2c46e075c8bf9f1607a635
Jenzi <jennydinh94@gmail.com>
2023-01-30 15:59:30 +0100

    Send data from controller to view in the About page

--- AnimalAdoption/Controllers/AboutController.cs
+++ AnimalAdoption/Controllers/AboutController.cs
@@ -14,6 +14,13 @@ namespace AnimalAdoption.Controllers
         [Route("/about_us")]
         public IActionResult Index()
         {
+            // Send data from the controller to the view
+            var aboutTitle = "How animals end up with us";
+            var aboutText = "Animals come to us for a variety of reasons. Some animals are abandoned by their owners, some of them have no where to go when their owner pass away and some of the animals are found on the streets.";
+
+            ViewBag.aboutTitle = aboutTitle;
+            ViewData["Text"] = aboutText;
+
             return View();
         }
     }
